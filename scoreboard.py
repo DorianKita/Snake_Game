859db66d101a4e728aaaf39ababba9a06711828d
Dorian Kita <dorian.kita90@gmail.com>
2024-11-29 20:51:42 +0100

    added wall collision detection

--- scoreboard.py
+++ scoreboard.py
@@ -7,13 +7,19 @@ class Scoreboard(Turtle):
         self.penup()
         self.hideturtle()
         self.color('white')
-        self.goto(x=0, y=280)
-        self.write(arg=f"Score: {self.score}", align='center', font=('Arial', 12,'bold'))
+        self.goto(x=0, y=270)
+        self.write(arg=f"Score: {self.score}", align='center', font=('Courier', 24,'bold'))
 
 
 
     def show_score(self):
         self.score +=1
         self.clear()
-        self.write(arg=f"Score {self.score}", align='center', font=('Arial', 12,'bold'))
+        self.write(arg=f"Score {self.score}", align='center', font=('Courier', 24,'bold'))
+
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write(arg=f"GAME OVER", align='center', font=('Courier', 24,'bold'))
+
 
--- snake_game.py
+++ snake_game.py
@@ -29,10 +29,14 @@ while game_is_on:
 
     snake.move()
 
+    #detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
         scoreboard.show_score()
 
-
+    #detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
 
 screen.exitonclick()
