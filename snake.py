c43eef41d7ecadff910bc2d3edf85b9f14bbba4e
Dorian Kita <dorian.kita90@gmail.com>
2024-11-29 19:37:02 +0100

    created control over snake

--- snake.py
+++ snake.py
@@ -1,6 +1,11 @@
 from turtle import Turtle
 
 STARTING_POSITIONS = [(0,0),(-20,0),(-40,0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 class Snake:
     def __init__(self):
@@ -15,7 +20,6 @@ class Snake:
             new_segment.color('white')
             new_segment.goto(position)
             self.segments.append(new_segment)
-        self.generated = True
 
 
 
@@ -24,4 +28,20 @@ class Snake:
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(x=new_x, y=new_y)
-        self.segments[0].forward(20)
+        self.segments[0].forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.segments[0].heading() != DOWN:
+            self.segments[0].setheading(UP)
+
+    def down(self):
+        if self.segments[0].heading() != UP:
+            self.segments[0].setheading(DOWN)
+
+    def right(self):
+        if self.segments[0].heading() != LEFT:
+            self.segments[0].setheading(RIGHT)
+
+    def left(self):
+        if self.segments[0].heading() != RIGHT:
+            self.segments[0].setheading(LEFT)
