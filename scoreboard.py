8a3653c662429eb33e049ecd3841d74c73c48c3d
Dorian Kita <dorian.kita90@gmail.com>
2024-12-09 22:38:37 +0100

    added saving highscore to a file

--- scoreboard.py
+++ scoreboard.py
@@ -1,10 +1,14 @@
 from turtle import Turtle
 
+
+
+
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open('data.txt') as data:
+            self.high_score = int(data.read())
         self.penup()
         self.hideturtle()
         self.color('white')
@@ -19,10 +23,11 @@ class Scoreboard(Turtle):
         self.write(arg=f"Score: {self.score} High Score: {self.high_score}", align='center', font=('Courier', 24,'bold'))
 
 
-
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open('data.txt', mode='w') as data:
+                data.write(f'{self.high_score}')
         self.score = 0
         self.clear()
         self.write(arg=f"Score: {self.score} High Score: {self.high_score}", align='center',
